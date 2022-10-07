0f3a4bc0e7762e5fc3b9420299ff67dd27b31929
rkkPersonal <17635841699@163.com>
2022-10-08 02:03:57 +0800

    add http server

--- src/main/java/com/netty/study/method/HttpMethod.java
+++ src/main/java/com/netty/study/method/HttpMethod.java
@@ -1,10 +1,18 @@
 package com.netty.study.method;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+
 /**
  * @author Steven
  * @date 2022年10月08日 1:55
  */
 
 public interface HttpMethod {
+
+    void response(ChannelHandlerContext ctx, FullHttpResponse response);
+
+    void request(ChannelHandlerContext ctx, FullHttpRequest request);
 
 }
--- src/main/java/com/netty/study/method/PostMethod.java
+++ src/main/java/com/netty/study/method/PostMethod.java
@@ -1,8 +1,22 @@
 package com.netty.study.method;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+
 /**
  * @author Steven
  * @date 2022年10月08日 1:56
  */
 public class PostMethod implements HttpMethod{
+
+    @Override
+    public void response(ChannelHandlerContext ctx, FullHttpResponse response) {
+
+    }
+
+    @Override
+    public void request(ChannelHandlerContext ctx, FullHttpRequest request) {
+
+    }
 }
